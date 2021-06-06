8fd4bf1c3e889e13e4c0ab49cfdc58bc9340848c
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-06-05 18:16:35 -0600

    setup User and setupChannels

--- app/src/main/java/com/example/chatdemo/ui/channel/ChannelFragment.kt
+++ app/src/main/java/com/example/chatdemo/ui/channel/ChannelFragment.kt
@@ -1,21 +1,100 @@
 package com.example.chatdemo.ui.channel
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.chatdemo.R
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.example.chatdemo.databinding.FragmentChannelBinding
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.models.Channel
+import io.getstream.chat.android.client.models.Filters
+import io.getstream.chat.android.client.models.User
+import io.getstream.chat.android.ui.channel.list.header.ChannelListHeaderView
+import io.getstream.chat.android.ui.channel.list.header.viewmodel.ChannelListHeaderViewModel
+import io.getstream.chat.android.ui.channel.list.header.viewmodel.bindView
+import io.getstream.chat.android.ui.channel.list.viewmodel.ChannelListViewModel
+import io.getstream.chat.android.ui.channel.list.viewmodel.bindView
+import io.getstream.chat.android.ui.channel.list.viewmodel.factory.ChannelListViewModelFactory
 
 class ChannelFragment : Fragment() {
 
+    private val args: ChannelFragmentArgs by navArgs()
+
+    private var _binding: FragmentChannelBinding?= null
+    private val binding get() = _binding!!
+
+    private val client = ChatClient.instance()
+    private lateinit var user: User
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_channel, container, false)
+        _binding = FragmentChannelBinding.inflate(inflater, container, false)
+
+        setupUser()
+        setupChannels()
+        return binding.root
     }
 
+
+
+    private fun setupUser() {
+        if (client.getCurrentUser() == null){
+            user = if(args.chatUser.firstName.contains("Maikol")){
+                User(
+                    id = args.chatUser.username,
+                    extraData = mutableMapOf(
+                        "name" to args.chatUser.firstName,
+                        "country" to "Costa Rica",
+                        "image" to "https://yt3.ggpht.com/ytc/AAUvwniNg3lwIeJ-ybvA1xuWBEzLoYA5KPxnKrojub0zhg=s900-c-k-c0x00ffffff-no-rj"
+                    ),
+                )
+            } else{
+                User(
+                    id = args.chatUser.username,
+                    extraData = mutableMapOf(
+                        "name" to args.chatUser.firstName
+                    )
+                )
+            }
+            val token = client.devToken(user.id)
+            client.connectUser(
+                user,
+                token
+            ).enqueue { result ->
+                if(result.isSuccess) {
+                    Log.d("ChannelFragment", "Success Connecting the User")
+                } else {
+                    Log.d("ChannelFragment", result.error().message.toString())
+                }
+
+            }
+        }
+    }
+    private fun setupChannels() {
+        val filters = Filters.and(
+            Filters.eq("type", "messaging"),
+            Filters.`in`("members", listOf(client.getCurrentUser()!!.id))
+        )
+        val viewModelFactory = ChannelListViewModelFactory(
+                filters,
+            ChannelListViewModel.DEFAULT_SORT
+        )
+        val listViewModel: ChannelListViewModel by viewModels { viewModelFactory }
+        val listHeaderViewModel: ChannelListHeaderViewModel by viewModels()
+
+        listHeaderViewModel.bindView(binding.channelListHeaderView, viewLifecycleOwner)
+            listViewModel.bindView(binding.channelsView, viewLifecycleOwner)
+
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
